ee1b0ca2e9f5216c969974f6dcbd7a8414caebe1
Vivek Pradhan <thepradhan4u@gmail.com>
2024-07-05 01:46:38 +0000

    variables

---
+++ 01Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 221;
+let accountName = "Vivek";
+var accountCity = "Jeypore";
+accountEmail = "vivekpradhan.work@gmail.com"
+
+accountName = "Vivek Pradhan"
+
+console.table([accountId, accountName, accountCity, accountEmail])
