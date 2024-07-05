3076d61ecc6ea84a98f69f2978b49fadc37d284f
Vivek Pradhan <thepradhan4u@gmail.com>
2024-07-05 01:55:41 +0000

    Conversion of datatypes

---
+++ 01Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "45"
+
+console.log(typeof(score))
+
+const valueinNumber = Number(score)
+
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+
+let isLoggedin = 1
+
+let booleanisloggedin = Boolean(isLoggedin)
+console.log(booleanisloggedin)
+
+let someNum = 45
+
+let stringNumber = String(someNum)
+console.log(typeof stringNumber)
